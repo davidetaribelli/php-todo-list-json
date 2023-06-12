7e53246c092280d98a0725fc9d3ada7e2de36e1c
davidetaribelli <davide.taribelli@gmail.com>
2023-06-12 14:41:33 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ToDo List</title>
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.4.0/axios.min.js"></script>
+</head>
+
+<body>
+    <div id="app">
+        <h1>{{message}}</h1>
+    </div>
+
+
+    <script src="./js/main.js"></script>
+</body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,9 @@
+const {createApp} = Vue;
+
+createApp({
+    data(){
+        return{
+            message: 'Benvenuto nella pagina', 
+        }
+    }
+}).mount('#app')
